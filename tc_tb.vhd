a02bb1db09a0ada3ab8ea61805f238c99c25a671
kkm213 <kkm213@korea.ac.kr>
2022-08-08 14:25:25 +0900

    upload

--- tc_tb.vhd
+++ tc_tb.vhd
@@ -16,14 +16,14 @@ architecture arch_tc_tb of tc_tb is
 begin 
     map0 : tc port map (cls_s,rst_s,G_s,Y_s,R_s);
 clocking : process begin
-    clk_s <= '0'
+    clk_s <= '0';
     wait for 10 ns;
     clk_s <= '1';
     wait for 20 ns;
 end process;
 
 reseting : process begin
-    rst_s <= '1'
+    rst_s <= '1';
     wait for 5 ns;
     rst_s <= '0';
     wait for 995 ns;
